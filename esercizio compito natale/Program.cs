c9bb9ac5ad5abd9716ceede0c8a2fc9a5a2caf92
alice <castaldialicee@gmail.com>
2024-01-09 15:59:53 +0100

    Aggiungere i file di progetto.

---
+++ esercizio compito natale/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_compito_natale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vettore vettore1 = new Vettore(2, 2);
+            Vettore vettore2= new Vettore(3,3);
+          // Vettore vettore3= new Vettore();
+           // vettore3 = Vettore.Prase("4; 4");
+
+            //somma tra il vettore 1 e 2
+            vettore1.Somma(vettore2);
+            
+
+        }
+    }
+}
---
+++ esercizio compito natale/Vettore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_compito_natale
+{
+    internal class Vettore
+    {
+        public Vettore(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public void Somma(Vettore vettore)
+        {
+            X += vettore.X;
+            Y += vettore.Y;
+        }
+    }
+}
